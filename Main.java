fe0ef44f25067989bc98b81a4df5e5f75ab998d8
mukeshkumarku <mukesh.ssvm@gmail.com>
2025-12-27 13:42:38 +0530

    beautify some code

--- Main.java
+++ Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!hhhhhhhh222");
                 System.out.println("Hello, World!hhhhhhhh222");
-
-                        System.out.println("Hello, World!hhhhhhhh3333");
-
-                                System.out.println("Hello, World!hhhhhhhh44444");
-
+                System.out.println("Hello, World!hhhhhhhh3333");
+                System.out.println("Hello, World!hhhhhhhh44444");
         System.out.println("Hello, World!hhhhhhhh5555555");
 
        Student student = new Student();
