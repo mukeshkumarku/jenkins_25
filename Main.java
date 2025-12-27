5153f09935f194be821cd822d49beaa9996a5514
mukeshkumarku <mukesh.ssvm@gmail.com>
2025-12-27 13:14:29 +0530

    Refactor output statements in Main.java for consistency5555

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!hhhhhhhh222");
+                System.out.println("Hello, World!hhhhhhhh222");
+
+                        System.out.println("Hello, World!hhhhhhhh3333");
+
+                                System.out.println("Hello, World!hhhhhhhh44444");
+
+        System.out.println("Hello, World!hhhhhhhh5555555");
+
     }
 }
