9529324fa46cd7094d954b07a4254a7f98cc479c
mukeshkumarku <mukesh.ssvm@gmail.com>
2025-12-27 13:50:26 +0530

    right code commit

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
                 System.out.println("Hello, World!hhhhhhhh222");
                 System.out.println("Hello, World!hhhhhhhh3333");
                 System.out.println("Hello, World!hhhhhhhh44444");
-        System.out.println("Hello, World!hhhhhhhh5555555")
+        System.out.println("Hello, World!hhhhhhhh5555555");
 
        Student student = new Student();
         student.study();
